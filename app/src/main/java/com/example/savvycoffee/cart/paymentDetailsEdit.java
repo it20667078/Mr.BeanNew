23f3a63ff6c6aec7bccac710c10ae9c2fa947da2
SahanDhananjayarox <it20667078@my.sliit.lk>
2022-05-09 22:21:50 +0530

    added xml files and other necessary files

---
+++ app/src/main/java/com/example/savvycoffee/cart/paymentDetailsEdit.java
@@ -0,0 +1,16 @@
+package com.example.savvycoffee.cart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.savvycoffee.R;
+
+public class paymentDetailsEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_details_edit);
+    }
+}
